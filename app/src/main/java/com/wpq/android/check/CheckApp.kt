f8f9c962f4b57391421d38d74630411cef51bcc7
武普泉 <wupuquan@dankegongyu.com>
2020-09-01 19:11:01 +0800

    init

---
+++ app/src/main/java/com/wpq/android/check/CheckApp.kt
@@ -0,0 +1,19 @@
+package com.wpq.android.check
+
+import android.app.Application
+import android.content.Context
+
+/**
+ * @author wupuquan
+ * @date 2020/9/1 19:08
+ */
+class CheckApp : Application() {
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
